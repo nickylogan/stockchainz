f38d53921a5b97f718446119f43ca0414daa3f80
NickyLogan <laurentiusdominick@yahoo.com>
2019-07-03 20:04:54 +0700

    Updates

---
+++ precache-manifest.67c81e4a3a51cdd4ba7eaf47678302be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8213ecfb08d44de1d344bc425f52802e",
+    "url": "/market1-app/index.html"
+  },
+  {
+    "revision": "ad2d9e5471da11ef3526",
+    "url": "/market1-app/static/css/2.dc646829.chunk.css"
+  },
+  {
+    "revision": "310ea823b050c541f535",
+    "url": "/market1-app/static/css/main.6fe004e0.chunk.css"
+  },
+  {
+    "revision": "ad2d9e5471da11ef3526",
+    "url": "/market1-app/static/js/2.7870c61a.chunk.js"
+  },
+  {
+    "revision": "310ea823b050c541f535",
+    "url": "/market1-app/static/js/main.df7fb9f3.chunk.js"
+  },
+  {
+    "revision": "987c52110871de073b9f",
+    "url": "/market1-app/static/js/runtime~main.919ea230.js"
+  }
+]);
